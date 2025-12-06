6a9454768d9f6686bb82ad84a13e673f1497aef9
Mohana2404 <mohanadharshini2404@gmail.com>
2025-12-06 15:15:26 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ViewStories from './viewStories.jsx'
+import ViewStories from './ViewStories.jsx'
 import Profile from './Profile.jsx'
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 
